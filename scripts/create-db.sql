32a7b7814c534c9bd1ee1d7ee3bd617b0a56aecc
cwalternicolas <nicolas.correa@mercadolibre.com>
2021-10-23 11:49:59 -0300

    Database scripts added

---
+++ scripts/create-db.sql
@@ -0,0 +1,43 @@
+CREATE TABLE `administrators` (
+  `id_administrator` int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `name` char(50) NOT NULL,
+  `lastname` char(50) NOT NULL,
+  `email` char(50) NOT NULL,
+  `password` char(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `students` (
+  `id_student` int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `id_career` int UNSIGNED NOT NULL,
+  `identifier` int UNSIGNED NOT NULL,
+  `name` char(50) NOT NULL,
+  `lastname` char(50) NOT NULL,
+  `email` char(50) NOT NULL,
+  `password` char(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `students_subjects` (
+  `id` int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `id_student` int UNSIGNED NOT NULL,
+  `id_subject` int UNSIGNED NOT NULL,
+  `status` char(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `careers` (
+  `id_career` int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `name` char(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `subjects` (
+  `id_subject` int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `name` char(80) NOT NULL,
+  `teacher` char(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `subjects_careers` (
+  `id` int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `id_subject` int UNSIGNED NOT NULL,
+  `id_career` int UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+COMMIT;
---
+++ scripts/init-db.sql
@@ -0,0 +1,148 @@
+INSERT INTO `subjects` (name, teacher) VALUES ('Informática I','Pedro Gonzales');
+INSERT INTO `subjects` (name, teacher) VALUES ('Álgebra y Geometría Analítica', 'Ricardo Centurión');				
+INSERT INTO `subjects` (name, teacher) VALUES ('Análisis Matemático I', 'Guillermo Schelotto');
+INSERT INTO `subjects` (name, teacher) VALUES ('Ingeniería y Sociedad', 'Martin Palermo');
+INSERT INTO `subjects` (name, teacher) VALUES ('Química General', 'Mauro Zarate');
+INSERT INTO `subjects` (name, teacher) VALUES ('Física I', 'Roberto Ayala');				
+INSERT INTO `subjects` (name, teacher) VALUES ('Sistemas de Representación', 'Guillermina Valdez');			
+INSERT INTO `subjects` (name, teacher) VALUES ('Informática II', 'Rocio Marengo');
+INSERT INTO `subjects` (name, teacher) VALUES ('Análisis Matemático II', 'Agustina Cherri');
+INSERT INTO `subjects` (name, teacher) VALUES ('Física II', 'Roberto Ayala');
+INSERT INTO `subjects` (name, teacher) VALUES ('Probabilidad y Estadística', 'Agustin Cherri');
+INSERT INTO `subjects` (name, teacher) VALUES ('Física Electrónica', 'Fabian Cubero');
+INSERT INTO `subjects` (name, teacher) VALUES ('Inglés Técnico Nivel I', 'Carlos Tevez');
+INSERT INTO `subjects` (name, teacher) VALUES ('Análisis de Señales y Sistemas', 'Roberto Chiellini');
+INSERT INTO `subjects` (name, teacher) VALUES ('Teoría de los Circuitos I', 'Marcelo Gallardo');
+INSERT INTO `subjects` (name, teacher) VALUES ('Técnicas Digitales I', 'Guillermina Valdez');
+INSERT INTO `subjects` (name, teacher) VALUES ('Dispositivos Electrónicos', 'Fabian Cubero');
+INSERT INTO `subjects` (name, teacher) VALUES ('Electrónica Aplicada I', 'Cristian Quercia');
+INSERT INTO `subjects` (name, teacher) VALUES ('Medios de Enlace', 'Micaela Rodriguez');
+INSERT INTO `subjects` (name, teacher) VALUES ('Inglés Técnico Nivel II', 'Carlos Tevez');
+INSERT INTO `subjects` (name, teacher) VALUES ('Técnicas Digitales II', 'Guillermina Valdez');
+INSERT INTO `subjects` (name, teacher) VALUES ('Medidas Electrónicas I', 'Nicolas Otamendi');
+INSERT INTO `subjects` (name, teacher) VALUES ('Teoría de los Circuitos II', 'Marcelo Gallardo');
+INSERT INTO `subjects` (name, teacher) VALUES ('Máquinas e Instalaciones Eléctricas', 'Roberto Carlos');
+INSERT INTO `subjects` (name, teacher) VALUES ('Legislación', 'Puntorero');
+INSERT INTO `subjects` (name, teacher) VALUES ('Sistemas de Comunicaciones', 'Joaquin Gonzales');
+INSERT INTO `subjects` (name, teacher) VALUES ('Electrónica Aplicada II', 'Cristian Quercia');
+INSERT INTO `subjects` (name, teacher) VALUES ('Seguridad, Higiene y Medio Ambiente', 'Marcela Boom');
+INSERT INTO `subjects` (name, teacher) VALUES ('Técnicas Digitales III', 'Guillermina Valdez');
+INSERT INTO `subjects` (name, teacher) VALUES ('Medidas Electrónicas II', 'Nicolas Otamendi');
+INSERT INTO `subjects` (name, teacher) VALUES ('Sistemas de Control', 'Nicolas Perez');
+INSERT INTO `subjects` (name, teacher) VALUES ('Electrónica Aplicada III', 'Cristian Quercia');	
+INSERT INTO `subjects` (name, teacher) VALUES ('Tecnología Electrónica', 'Fabian Correa');
+INSERT INTO `subjects` (name, teacher) VALUES ('Electrónica de Potencia', 'Cristian Quercia');
+INSERT INTO `subjects` (name, teacher) VALUES ('Organización Industrial', 'Aixa Hizto');
+INSERT INTO `subjects` (name, teacher) VALUES ('Práctica Supervisada', 'Javier Cañete');
+INSERT INTO `subjects` (name, teacher) VALUES ('Economía', 'Alberto Fernandez');
+INSERT INTO `subjects` (name, teacher) VALUES ('Proyecto Final', 'Pablo Echarri');
+INSERT INTO `subjects` (name, teacher) VALUES ('Aeronáutica I', 'Nestor Serravale');						
+INSERT INTO `subjects` (name, teacher) VALUES ('Química General', 'Fernando Bambi');							
+INSERT INTO `subjects` (name, teacher) VALUES ('Computación', 'Rocio Marengo');				
+INSERT INTO `subjects` (name, teacher) VALUES ('Aeronáutica II', 'Nestor Serravale');
+INSERT INTO `subjects` (name, teacher) VALUES ('Estructuras Aeronáuticas I', 'Julian Alvarez');	
+INSERT INTO `subjects` (name, teacher) VALUES ('Ciencia de los Materiales I', 'Sergio Romero');	
+INSERT INTO `subjects` (name, teacher) VALUES ('Mecánica', 'Nicolas Stella');	
+INSERT INTO `subjects` (name, teacher) VALUES ('Ciencia de los Materiales II', 'Sergio Romero');	
+INSERT INTO `subjects` (name, teacher) VALUES ('Matemáticas Aplicadas a la Aeronáutica', 'Nicolas Sandoval');	
+INSERT INTO `subjects` (name, teacher) VALUES ('Representación Gráfica en Aeronáutica', 'Barassi');
+INSERT INTO `subjects` (name, teacher) VALUES ('Sistemas del Avión', 'Aero Plane');	
+INSERT INTO `subjects` (name, teacher) VALUES ('Estructuras Aeronáuticas II', 'Julian Alvarez');
+INSERT INTO `subjects` (name, teacher) VALUES ('Electrotecnia', 'Luis Suarez');
+INSERT INTO `subjects` (name, teacher) VALUES ('Mecánica del Continuo', 'Nicolas Stella');
+INSERT INTO `subjects` (name, teacher) VALUES ('Termodinámica Técnica', 'Fabian Izco');	
+INSERT INTO `subjects` (name, teacher) VALUES ('Electrónica', 'Cristian Quercia');	
+INSERT INTO `subjects` (name, teacher) VALUES ('Mecánica de los Fluidos', 'Kevin Montero');	
+INSERT INTO `subjects` (name, teacher) VALUES ('Proyecto y Diseño Aerodinámico', 'Micaela Nilsen');
+INSERT INTO `subjects` (name, teacher) VALUES ('Estructuras Aeronáuticas III', 'Julian Alvarez');	
+INSERT INTO `subjects` (name, teacher) VALUES ('Mecanismos', 'Pedro Hiz');		
+INSERT INTO `subjects` (name, teacher) VALUES ('Aerodinámica Teórica', 'Nestor Serravale');	
+INSERT INTO `subjects` (name, teacher) VALUES ('Instrumentos y Mediciones', 'Sebastian Stra');	
+INSERT INTO `subjects` (name, teacher) VALUES ('Mecánica del Vuelo', 'Guillermo Fly');
+INSERT INTO `subjects` (name, teacher) VALUES ('Propulsión', 'Oscar Perez');	
+INSERT INTO `subjects` (name, teacher) VALUES ('Proyecto y Diseño de Estructuras Aeronáuticas', 'Gaston Guado');
+INSERT INTO `subjects` (name, teacher) VALUES ('Proceso de Fabricación de Aviones', 'Daniel Jorge');	
+INSERT INTO `subjects` (name, teacher) VALUES ('Normativa Aeronáutica', 'Puntorero');
+INSERT INTO `subjects` (name, teacher) VALUES ('Mantenimiento General de Aeronaves', 'Leonardo Vila');
+INSERT INTO `subjects` (name, teacher) VALUES ('Aeropuertos, Aeronavegación e Impacto Ambiental', 'Santiago Medina');	
+INSERT INTO `subjects` (name, teacher) VALUES ('Programación I', 'Alejandro Camba');			
+INSERT INTO `subjects` (name, teacher) VALUES ('Sistema de Procesamiento de Datos', 'Mauro Godin');			
+INSERT INTO `subjects` (name, teacher) VALUES ('Matemática', 'Nicolas Sandoval');					
+INSERT INTO `subjects` (name, teacher) VALUES ('Laboratorio de Computación I', 'Guillermo Rauch');		
+INSERT INTO `subjects` (name, teacher) VALUES ('Programación II', 'Alejandro Camba');
+INSERT INTO `subjects` (name, teacher) VALUES ('Arquitectura y Sistemas Operativos', 'Mauro Godin');
+INSERT INTO `subjects` (name, teacher) VALUES ('Laboratorio de Computación II', 'Guillermo Rauch');
+INSERT INTO `subjects` (name, teacher) VALUES ('Metodología de la Investigación', 'Ines Perez');			
+INSERT INTO `subjects` (name, teacher) VALUES ('Programación III', 'Alejandro Camba');
+INSERT INTO `subjects` (name, teacher) VALUES ('Organización Contable de la Empresa', 'Laura Gutierrez');
+INSERT INTO `subjects` (name, teacher) VALUES ('Organización Empresarial', 'Laura Gutierrez');
+INSERT INTO `subjects` (name, teacher) VALUES ('Elementos de la Investigación Operativa', 'Fabricio Coloccini');
+INSERT INTO `subjects` (name, teacher) VALUES ('Laboratorio de Computación III', 'Mauro Godin');
+INSERT INTO `subjects` (name, teacher) VALUES ('Metodología de Sistemas', 'Ines Perez');
+INSERT INTO `subjects` (name, teacher) VALUES ('Diseño y Administración de Bases de Datos', 'Guillermo Rauch');		
+INSERT INTO `subjects` (name, teacher) VALUES ('Laboratorio de Computación IV', 'Alejandro Camba');
+
+-- -- ing aeronautica
+-- Aeronáutica I				
+-- -- Álgebra y Geometría Analítica				
+-- -- Análisis Matemático I				
+-- -- Física I				
+-- Química General				
+-- -- Ingeniería y Sociedad				
+-- -- Sistemas de Representación				
+-- Computación				
+-- Aeronáutica II
+-- Probabilidad y Estadística
+-- Estructuras Aeronáuticas I	
+-- -- Análisis Matemático II
+-- -- Física II
+-- Ciencia de los Materiales I	
+-- Mecánica	
+-- Ciencia de los Materiales II	
+-- Matemáticas Aplicadas a la Aeronáutica	
+-- Representación Gráfica en Aeronáutica
+-- Sistemas del Avión	
+-- -- Economía	
+-- Estructuras Aeronáuticas II
+-- Electrotecnia	
+-- Mecánica del Continuo
+-- Termodinámica Técnica	
+-- Electrónica	
+-- Mecánica de los Fluidos	
+-- Proyecto y Diseño Aerodinámico
+-- Estructuras Aeronáuticas III	
+-- Mecanismos	
+-- -- Sistemas de Control	
+-- Aerodinámica Teórica	
+-- Instrumentos y Mediciones	
+-- -- Organización Industrial	
+-- Mecánica del Vuelo
+-- Propulsión	
+-- Proyecto y Diseño de Estructuras Aeronáuticas
+-- Proceso de Fabricación de Aviones	
+-- Normativa Aeronáutica
+-- Mantenimiento General de Aeronaves
+-- Aeropuertos, Aeronavegación e Impacto Ambiental	
+-- -- Legislacion
+
+-- -- TUP
+-- Programación I			
+-- Sistema de Procesamiento de Datos			
+-- Matemática			
+-- -- Inglés I			
+-- Laboratorio de Computación I			
+-- Programación II
+-- Arquitectura y Sistemas Operativos
+-- -- Estadística
+-- -- Inglés II
+-- Laboratorio de Computación II
+-- Metodología de la Investigación			
+-- Programación III
+-- Organización Contable de la Empresa
+-- Organización Empresarial
+-- Elementos de la Investigación Operativa
+-- Laboratorio de Computación III
+-- Metodología de Sistemas
+-- Diseño y Administración de Bases de Datos
+-- -- Legislación			
+-- Laboratorio de Computación IV
