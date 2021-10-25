61b9e4ce2d31e2242a0f05b2968dbbaf71e62101
Ivan Baigorria <unbaigorria.ivan@gmail.com>
2021-10-25 18:33:16 -0300

    Add layout and UI dependencies

---
+++ web/html/Layout.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- jquery datatables CSS -->
+    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css"/>
+    <!-- main CSS -->
+    <link rel="stylesheet" type="text/css" href="../assets/stylesheets/main.css"/>
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!-- jquery -->
+    <script src="../assets/javascript/jquery-3.6.0.js"></script>
+    <!-- jquery datatables -->
+    <script src="//cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+    <!-- font awesome -->
+    <script src="https://kit.fontawesome.com/062112aa49.js" crossorigin="anonymous"></script>
+
+    <title>High School Inscription</title>
+  </head>
+  <body>
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="../controllers/indexController.php">High School Inscription</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="../controllers/indexController.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../controllers/careersList.php">Carreras</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../controllers/administratorsList.php">Administradores</a>
+            </li>
+            <!-- Con esto nada todavia
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+              </li>
+            -->
+          </ul>
+          <a href="../controllers/logoutController.php" class="btn">Cerrar sesión</a>
+        </div>
+      </div>
+    </nav>
+    <div id="content">
+      {{content}}
+    </div>
+  </body>
+</html>
