2625c8a82104b537f0666a89edb382738b9b2a0f
SchleicherConfalonieriN <76877741+SchleicherConfalonieriN@users.noreply.github.com>
2022-11-11 17:27:54 -0300

    quitar el legajo de opciones de edicion

--- web/html/EditStudentView.php
+++ web/html/EditStudentView.php
@@ -5,28 +5,21 @@
     <div id="content">
       <div class="card">
         <div class="card-header">
-          <h5 class="card-title">Editar alumno</h5>
+          <h5 class="card-title">Editar materia</h5>
         </div>
         <form action="" method="POST">
           <div class="card-body">
             <div class="mb-3">
               <label for="name" class="form-label">Nombre</label>
-              <input type="text" name="name" value="<?=htmlentities($this->student['name'])?>" class="form-control" id="name" maxlength="50" required>
+              <input type="text" name="name" value="<?=htmlentities($this->subject['name'])?>" maxlength="80" class="form-control" id="name" required>
             </div>
             <div class="mb-3">
-              <label for="lastname" class="form-label">Apellido</label>
-              <input type="text" name="lastname" value="<?=htmlentities($this->student['lastname'])?>" class="form-control" id="lastname" maxlength="50" required>
+              <label for="teacher" class="form-label">Profesor</label>
+              <input type="text" name="teacher" value="<?=htmlentities($this->subject['teacher'])?>" maxlength="50" class="form-control" id="teacher" required>
             </div>
-            <div class="mb-3">
-              <label for="email" class="form-label">Email</label>
-              <input type="email" name="email" value="<?=htmlentities($this->student['email'])?>" class="form-control" id="email" maxlength="50" required>
-            </div>
-            <div class="mb-3">
-              <label for="identifier" class="form-label">Numero de legajo</label>
-              <input type="number" name="identifier" value="<?=htmlentities($this->student['identifier'])?>" class="form-control" id="identifier" min="1" required>
-            </div>
-            <input type="hidden" name="id" value="<?=htmlentities($this->student['id_student'])?>">
           </div>
+          <input type="hidden" name="id" value="<?=htmlentities($this->subject['id_subject'])?>">
+          <input type="hidden" name="careerId" value="<?=htmlentities($_GET['careerId'])?>">
           <div class="card-footer">
             <button type="submit" class="btn btn-success">Guardar</button>
           </div>
