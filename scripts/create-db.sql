0500fc9cd1e3a4a6115e916c8403e07d6814e7ff
Nicolas Correa <cwalternicolas@gmail.com>
2022-11-13 14:19:37 -0300

    FK added to tables

--- scripts/create-db.sql
+++ scripts/create-db.sql
@@ -6,6 +6,11 @@ CREATE TABLE `administrators` (
   `password` char(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `careers` (
+  `id_career` int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `name` char(50) NOT NULL UNIQUE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `students` (
   `id_student` int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `id_career` int UNSIGNED NOT NULL,
@@ -13,19 +18,10 @@ CREATE TABLE `students` (
   `name` char(50) NOT NULL,
   `lastname` char(50) NOT NULL,
   `email` char(50) NOT NULL UNIQUE,
-  `password` char(64) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `students_subjects` (
-  `id` int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `id_student` int UNSIGNED NOT NULL,
-  `id_subject` int UNSIGNED NOT NULL,
-  `status` char(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `careers` (
-  `id_career` int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `name` char(50) NOT NULL UNIQUE
+  `password` char(64) NOT NULL,
+  CONSTRAINT fk_student_career
+    FOREIGN KEY (id_career) 
+        REFERENCES careers(id_career)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `subjects` (
@@ -34,10 +30,31 @@ CREATE TABLE `subjects` (
   `teacher` char(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `students_subjects` (
+  `id` int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `id_student` int UNSIGNED NOT NULL,
+  `id_subject` int UNSIGNED NOT NULL,
+  `status` char(20) NOT NULL,
+  CONSTRAINT fk_student_subject
+    FOREIGN KEY (id_student) 
+        REFERENCES students(id_student),
+  CONSTRAINT fk_subject_subject
+    FOREIGN KEY (id_subject) 
+      REFERENCES subjects(id_subject)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
 CREATE TABLE `subjects_careers` (
   `id` int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `id_subject` int UNSIGNED NOT NULL,
-  `id_career` int UNSIGNED NOT NULL
+  `id_career` int UNSIGNED NOT NULL,
+  CONSTRAINT fk_subject_career
+    FOREIGN KEY (id_subject) 
+        REFERENCES subjects(id_subject),
+  CONSTRAINT fk_career_career
+    FOREIGN KEY (id_career) 
+        REFERENCES careers(id_career)     
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 COMMIT;
